adb4ba4815048c7f0e3a56f40cfed7f54fc843ef
Koji Inoue <inouekoji2013@gmail.com>
2025-07-30 11:27:30 +0900

    Add test script for VapGPT model with mic input
    Introduces test_vap_mc_mic.py to demonstrate usage of the VapGPT model with microphone input. The script sets up the Maai class in 'vap_mc' mode, processes audio from the mic, and outputs results to a console bar.

---
+++ test_scripts/vap_mc/test_vap_mc_mic.py
@@ -0,0 +1,38 @@
+"""
+This script is an example of how to use the VapGPT model.
+"""
+
+import sys
+import os
+
+# プロジェクトルートをパスに追加
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src/')))
+
+from maai import Maai, MaaiInput, MaaiOutput
+
+def test_vap():
+
+    output = MaaiOutput.ConsoleBar()
+
+    maai = Maai(
+        mode="vap_mc",
+        language="jp",
+        frame_rate=10,
+        context_len_sec=5,
+        audio_ch1=MaaiInput.Mic(),
+        audio_ch2=MaaiInput.Zero(),
+        device="cpu",
+        force_download=True
+    )
+
+    maai.start_process()
+
+    while True:
+        result = maai.get_result()
+        output.update(result)
+        
+if __name__ == "__main__":
+    try:
+        test_vap()
+    except KeyboardInterrupt:
+        print("Ending the test script.")
